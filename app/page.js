70f2a816b5dce306efd2e1c3f1825b3766c4f7f4
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-05-23 15:25:58 +0800

    chore: hi

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-
+// ako si mark
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-row items-center justify-center gap-20 bg-white">
